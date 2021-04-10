e3f9f39bc2e35cacb881512c4cfb4282229972d0
Laughlin Elkind <lelkind@gmail.com>
2021-04-10 16:11:11 -0700

    Add login form and route

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ db = SQLAlchemy(app)
 
 
 #create login manager later
-login_manager = LoginManager()
-login_manager.init_app(app)
+#login_manager = LoginManager()
+#login_manager.init_app(app)
 import routes
 #import models later
--- routes.py
+++ routes.py
@@ -1,10 +1,19 @@
 from app import app, db
 from flask import request, render_template, flash, redirect, url_for
 #from models import User, Recipes, Ingredients, RecipeIngredients, Ratings
-#from forms import <all the forms I need to make>
+from forms import LoginForm
 from werkzeug.urls import url_parse
 
 #Landing page
 @app.route('/')
+@app.route('/index')
 def index():
-  return render_template("index.html")
+  return render_template("index.html", title="Home")
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+  form=LoginForm()
+  if form.validate_on_submit():
+    flash(f'Login requested for user {form.username.data}, remember_me = {form.remember_me.data}.')
+    return redirect(url_for('index'))
+  return render_template('login.html', title="Sign In", form=form)
