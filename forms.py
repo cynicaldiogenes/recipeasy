f1a6d1319699e431078b145bd2a14443373adc21
Laughlin Elkind <lelkind@gmail.com>
2021-04-11 10:53:14 -0700

    Add new acct registration form

--- forms.py
+++ forms.py
@@ -1,9 +1,43 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField, SelectMultipleField
+from wtforms.fields.html5 import URLField, EmailField
+from wtforms.validators import DataRequired, EqualTo
 
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
   password = PasswordField('Password', validators=[DataRequired()])
   remember_me = BooleanField('Remember Me')
   submit = SubmitField('Sign In')
+
+class RegisterForm(FlaskForm):
+  username = StringField('Username', validators=[DataRequired()])
+  email = EmailField('E-mail address', validators=[DataRequired()])
+  password = PasswordField('Password', 
+    validators=[EqualTo('password_confirm', message='Passwords must match')])
+  password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+  submit = SubmitField('Register Account')
+
+class IngredientForm(FlaskForm):
+  name = StringField('Ingredient Name', validators=[DataRequired()]),
+  calories_per = IntegerField('Calories per', validators=[DataRequired()]),
+  unit_type = StringField('Measurement unit', validators=[DataRequired()]),
+  notes = StringField('Notes')
+  submit = SubmitField('Add Ingredient')
+
+class RecipeForm(FlaskForm):
+  name = StringField('Recipe Name', validators=[DataRequired()]),
+  instructions = TextAreaField('Instructions', validators=[DataRequired()]),
+  source = URLField('Source')
+  # Need to figure out if I need to embed RecipeIngredientForm or something
+  submit = SubmitField('Add Recipe')
+
+class RecipeIngredientForm(FlaskForm):
+  recipe_ingredients = SelectMultipleField('Ingredients', validators=[DataRequired()])
+  # Need to figure out how to also add quantities
+  submit = SubmitField('Add Ingredients')
+
+  #From https://stackoverflow.com/questions/31619747/dynamic-select-field-using-wtforms-not-updating/31619945#31619945
+  def __init__(self, *args, **kwargs):
+    super(RecipeIngredientForm, self).__init__(*args, **kwargs)
+    self.recipe_ingredients.choices = [ 
+      (ingredient.id, ingredient.name) for ingrdient in Ingredient.query.order_by(Ingredient.name)]
--- routes.py
+++ routes.py
@@ -1,7 +1,7 @@
 from app import app, db
 from flask import request, render_template, flash, redirect, url_for
 #from models import User, Recipes, Ingredients, RecipeIngredients, Ratings
-from forms import LoginForm
+from forms import LoginForm, RegisterForm, IngredientForm, RecipeForm, RecipeIngredientForm
 from werkzeug.urls import url_parse
 
 #Landing page
@@ -17,3 +17,11 @@ def login():
     flash(f'Login requested for user {form.username.data}, remember_me = {form.remember_me.data}.')
     return redirect(url_for('index'))
   return render_template('login.html', title="Sign In", form=form)
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+  form=RegisterForm()
+  if form.validate_on_submit():
+    flash(f'Creating user {form.username.data} at {form.email.data}! Just kidding, this is a placeholder')
+    return redirect(url_for('login'))
+  return render_template('register.html', title="Create an account", form=form)
