a05246cd44aca444692995c53a3b0363a018768c
Laughlin Elkind <lelkind@gmail.com>
2021-04-12 15:20:19 -0700

    Add database migrations

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField, TextAreaField, SelectMultipleField
 from wtforms.fields.html5 import URLField, EmailField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email, ValidationError
 
 class LoginForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
@@ -11,11 +11,21 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
   username = StringField('Username', validators=[DataRequired()])
-  email = EmailField('E-mail address', validators=[DataRequired()])
+  email = EmailField('E-mail address', validators=[DataRequired(), Email()])
   password = PasswordField('Password', 
     validators=[EqualTo('password_confirm', message='Passwords must match')])
   password_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
   submit = SubmitField('Register Account')
+
+  def validate_username(self, username):
+    user = User.query.filter_by(username=username.data).first()
+    if user is not None:
+      raise ValidationError('Please use a different username.')
+  
+  def validate_email(self, email):
+    user = User.query.filter_by(email=email.data).first()
+    if user is not None:
+      raise ValidationError('Please use a different email address.')
 
 class IngredientForm(FlaskForm):
   name = StringField('Ingredient Name', validators=[DataRequired()]),
